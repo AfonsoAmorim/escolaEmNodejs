2adec1a769ab28e14ff677d5954a53b4b1043268
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-06-17 20:08:39 -0300

    novas functions

--- entidades/classes.js
+++ entidades/classes.js
@@ -1,4 +1,5 @@
 let mediaAlunos = require("../funcoes/functions.js");
+let feedbackProfessorA = require("../funcoes/functions.js");
 
 class Professor{
     constructor(nome,idade){
@@ -24,3 +25,4 @@ let alunoAds = new Aluno("João Alves",23,5544,"ADS","3° Ano");
 console.log(professorTitular);
 console.log(alunoAds);
 console.log(`A média do aluno ${alunoAds.nome} é: `+ mediaAlunos(4,4,5,8));
+console.log(feedbackProfessorA(8));
--- funcoes/functions.js
+++ funcoes/functions.js
@@ -1,4 +1,27 @@
 let mediaFinal = function (a,b,c,d) {
     return (a+b+c+d)/4
 }
+
+let feedbackProfessor = function (avaliacaoProfessor) {//mensura qualidade do professor
+    
+    if(avaliacaoProfessor <= 4){
+        console.log(`Professor com avaliação baixa, necessário ação.Nota: ` + avaliacaoProfessor);
+    }else if(avaliacaoProfessor <=7){
+        console.log(`Professor com avaliação moderada, necessário ação.Nota: ` + avaliacaoProfessor);
+    }else{
+        console.log(`Professor com ótima avaliação.Nota: ` + avaliacaoProfessor);
+    }return;
+  
+}
+
+
+
+
+
+
+
+
+
+
 module.exports = mediaFinal;
+module.exports = feedbackProfessor;
