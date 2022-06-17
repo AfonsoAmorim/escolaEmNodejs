65f106d62513a23c88b613f73f351fb8f55b7248
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-06-17 19:46:56 -0300

    first commit

---
+++ entidades/classes.js
@@ -0,0 +1,26 @@
+let mediaAlunos = require("../funcoes/functions.js");
+
+class Professor{
+    constructor(nome,idade){
+        this.nome = nome;
+        this.idade = idade;
+        
+    }
+}
+
+class Aluno extends Professor{
+    constructor(nome,idade,registroAluno,classe,serieAno){
+        super(nome);
+        this.idade=idade;
+        this.registroAluno=registroAluno;
+        this.classe=classe;
+        this.serieAno=serieAno;
+    }
+}
+
+let professorTitular = new Professor("Leandro",44);
+let alunoAds = new Aluno("João Alves",23,5544,"ADS","3° Ano");
+
+console.log(professorTitular);
+console.log(alunoAds);
+console.log(`A média do aluno ${alunoAds.nome} é: `+ mediaAlunos(4,4,5,8));
---
+++ funcoes/functions.js
@@ -0,0 +1,4 @@
+let mediaFinal = function (a,b,c,d) {
+    return (a+b+c+d)/4
+}
+module.exports = mediaFinal;
